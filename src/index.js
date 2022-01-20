8cbb9d57a1fb447c8d8e192f8d8f1f82a9538db5
MalgorzataWisniewska <mwisniewska@interia.eu>
2022-01-20 17:52:13 +0100

    authentication: admin created, admin log in/out added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+
+//imports from firebase
 import {
   getFirestore,
   collection,
@@ -6,7 +8,7 @@ import {
   addDoc,
 } from "firebase/firestore";
 
-import { firebaseConfig } from "./config";
+//imports from storage
 import {
   getStorage,
   ref,
@@ -14,19 +16,31 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 
+//imports from auth
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+//import (from) files
+
+import { firebaseConfig } from "./config";
 import "./styles.css";
+
+//other imports
 
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/brands";
 
-const key = firebaseConfig.apiKey;
-
-// init firebase app and storage
+// init firebase app and storage and auth
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
+const auth = getAuth();
 
-//init serivices
+//init services
 const db = getFirestore();
 
 // connect to collection (ref)
@@ -65,24 +79,24 @@ const showData = (cat) => {
 };
 
 //form
-const formCont = document.getElementsByClassName("form")[0];
+const qFormCont = document.getElementsByClassName("form")[0];
 const showForm = document.getElementsByClassName("add")[0];
-const close = document.getElementsByClassName("close")[0];
 const form = document.getElementsByClassName("quest")[0];
 const subBtn = document.getElementsByClassName("submit")[0];
 
 showForm.addEventListener("click", () => {
-  formCont.classList.remove("no-show");
+  qFormCont.classList.remove("no-show");
 });
 
 //closing form window
-const closeForm = () => {
-  form.reset();
-  subBtn.textContent = "Submit";
-  formCont.classList.add("no-show");
+const closeForm = (currentForm) => {
+  currentForm.reset();
+  currentForm.parentElement.classList.add("no-show");
 };
 
-close.addEventListener("click", () => closeForm());
+Array.from(document.getElementsByClassName("close")).forEach((x) =>
+  x.addEventListener("click", () => closeForm(x.nextElementSibling))
+);
 
 //adding cats to the base
 
@@ -96,7 +110,10 @@ const uploadFiles = (file, cat) => {
     () => {
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
         addDoc(colRef, { ...cat, imgURL: downloadURL })
-          .then(() => closeForm())
+          .then(() => {
+            closeForm(form);
+            form.getElementsByTagName("button")[0].textContent = "Submit";
+          })
           .catch((err) => err.message);
       });
     }
@@ -116,6 +133,52 @@ form.addEventListener("submit", (e) => {
     adopted: e.target.adopted.value,
     notes: e.target.notes.value.trim(),
   };
-  subBtn.textContent = "Loading...";
+  subBtn.textContent = "Uploading...";
   uploadFiles(file, cat);
 });
+
+// ADMIN
+// creating admin account - DONE
+// createUserWithEmailAndPassword(auth, "admin@admin.com", "admin123")
+//   .then(() => console.log("Admin created"))
+//   .catch((err) => console.log(err.message));
+
+//show login form
+const showLog = document.getElementsByClassName("loginBtn")[0];
+const logoutBtn = document.getElementsByClassName("logout")[0];
+const logFormCont = document.getElementsByClassName("form")[1];
+
+showLog.addEventListener("click", (e) => {
+  logFormCont.classList.remove("no-show");
+});
+
+//login admin
+const logForm = document.getElementsByClassName("logForm")[0];
+logForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  showLog.classList.add("no-show");
+  const mail = logForm.login.value;
+  const password = logForm.password.value;
+  signInWithEmailAndPassword(auth, mail, password)
+    .then((cred) => {
+      console.log("Admin logged in!:", cred.user);
+      logoutBtn.classList.remove("no-show");
+      showForm.classList.remove("no-show");
+      closeForm(logForm);
+    })
+    .catch((err) => console.log(err.message));
+});
+
+//logout admin
+logoutBtn.addEventListener("click", (e) => {
+  logoutBtn.classList.add("no-show");
+  signOut(auth)
+    .then(() => {
+      console.log("Admin logged out!");
+      showLog.classList.remove("no-show");
+      showForm.classList.add("no-show");
+    })
+    .catch((err) => console.log(err.message));
+});
+
+//auth.currentUser
